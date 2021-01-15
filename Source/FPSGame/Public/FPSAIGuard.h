2efde40713ca9ae56288b80978e6c26873289001
alpcaca0v0 <1405772199@qq.com>
2021-01-15 21:34:39 +0800

    FPSGame
    no

---
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FPSAIGuard.generated.h"
+
+class UPawnSensingComponent;
+
+UENUM(BlueprintType)
+enum class EAIState : uint8
+{
+	Idle,
+
+	Suspicious,
+
+	Alerted
+
+};
+
+
+UCLASS()
+class FPSGAME_API AFPSAIGuard : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFPSAIGuard();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//Let the guard go on patrol
+	UPROPERTY(EditInstanceOnly, Category = "AI")
+	bool bPatrol;
+
+	UPROPERTY(EditInstanceOnly, Category = "AI", meta = (EditCondition = "bPatrol"))
+	AActor* FirstPatrolPoint;
+
+	//second of two patrol points to patrol between
+	UPROPERTY(EditInstanceOnly, Category = "AI", meta = (EditCondition = "bPatrol"))	
+	AActor* SecondPatrolPoint;
+
+	//The current point the actor is either moving to or standing at
+	AActor* CurrentPatrolPoint;
+
+	void MoveToNextPatrolPoint();
+
+
+	UPROPERTY(VisibleAnywhere,Category="Components")
+	UPawnSensingComponent* PawnSensingComp;
+	
+	UFUNCTION()
+	void OnSeenPawn(APawn* SeenPawn);
+
+	UFUNCTION()
+	void OnHeardPawn(APawn* NoiseInstigator, const FVector& Location, float Volume);
+
+	FRotator OriginalRotation;
+
+	void ResetOrientation();
+
+	FTimerHandle TimerHandle_ResetOrientation;
+
+	UPROPERTY(ReplicatedUsing=OnRep_GuardState)
+	EAIState GuardState;
+	
+	UFUNCTION()
+	void OnRep_GuardState();
+
+	void SetGuardState(EAIState NewState);
+
+	UFUNCTION(BlueprintImplementableEvent,Category="AI")
+	void OnStateChanged(EAIState NewState);
+
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const override;
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+};
